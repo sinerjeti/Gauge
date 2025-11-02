6551bdbd259971df95afde78cebca58ada99071f
azevt <azevt@DESKTOP-HGIRMOJ>
2025-11-02 23:29:38 +0300

    Добавьте файлы проекта.

---
+++ Gauge/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Gauge
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Gauge/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Gauge")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Gauge.Pages")]
